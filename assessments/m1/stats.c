77208708e12cc46ef9b835f6a2f1f37ba75b32c5
phansonloc1999 <phansonloc1999@gmail.com>
2021-10-01 09:18:04 +0700

    Add print array

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -19,25 +19,34 @@
  *
  */
 
-
-
 #include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void main()
+{
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                              114, 88, 45, 76, 123, 87, 25, 23,
+                              200, 122, 150, 90, 92, 87, 177, 244,
+                              201, 6, 12, 60, 8, 2, 5, 67,
+                              7, 87, 250, 230, 99, 3, 100, 90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  print_array(test, SIZE);
 }
 
 /* Add other Implementation File Code Here */
+void print_array(unsigned char *array, int size)
+{
+  printf("\n");
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
